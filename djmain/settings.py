5edc774d2cabc3d4d036b55de4a731f031c4bdd1
xFarisSx <faristaleek@gmail.com>
2023-01-26 17:50:48 +0300

    finishs

--- djmain/settings.py
+++ djmain/settings.py
@@ -30,6 +30,9 @@ ALLOWED_HOSTS = ['*']
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_ALL_ORIGINS = True # If this is used then `CORS_ALLOWED_ORIGINS` will not have any effect
 CORS_ALLOW_CREDENTIALS = True
+CORS_ORIGIN_WHITELIST = (
+'https://web-production-f609.up.railway.app/'
+)
 
 # Application definition
 
