9e1f4290b9b666cf0f36857105357bef664e2c6c
xFarisSx <faristaleek@gmail.com>
2023-01-26 17:57:10 +0300

    finishs

--- djmain/settings.py
+++ djmain/settings.py
@@ -28,9 +28,26 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 CORS_ORIGIN_ALLOW_ALL = True
-CORS_ALLOW_HEADERS = ['*']
+CORS_ALLOW_HEADERS = [
+'accept',
+'accept-encoding',
+'authorization',
+'content-type',
+'dnt',
+'origin',
+'user-agent',
+'x-csrftoken',
+'x-requested-with',
+]
 CORS_ALLOW_CREDENTIALS = True
-
+CORS_ALLOW_METHODS = [
+'DELETE',
+'GET',
+'OPTIONS',
+'PATCH',
+'POST',
+'PUT',
+]
 # Application definition
 
 INSTALLED_APPS = [
